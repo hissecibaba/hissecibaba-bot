2ee23100c1635e5d3d2e2e6428cf10046ca58faf
hissecibaba <aphobi@hotmail.com>
2026-04-12 10:42:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -831,6 +831,6 @@ start_services()
 
 if __name__ == "__main__":
     # Render Portu
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8020))
     # Local çalıştırma
     flask_app.run(host="0.0.0.0", port=port, debug=False)
